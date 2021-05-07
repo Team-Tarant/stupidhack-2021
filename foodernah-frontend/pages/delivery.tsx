78e7482e82ae2600a0d4d5d3d77f37ff1a1bdbbc
Joona Heikkilä <joona.o.heikkila@gmail.com>
2021-05-08 01:38:10 +0300

    Skriv bättre kod för Delivery sidan

--- foodernah-frontend/pages/delivery.tsx
+++ foodernah-frontend/pages/delivery.tsx
@@ -2,10 +2,10 @@ import Delivery from '../components/Delivery'
 import styles from '../styles/DeliveryPage.module.css'
 
 const DeliveryPage = () => {
-  const est = new Date(new Date().getTime() + 5 * 60 * 1000)
+  const est = new Date(new Date().getTime() + 1 * 60 * 1000)
   return (
     <div className={styles.container}>
-      <Delivery estimatedDelivery={est} />
+      <Delivery deliveryStarted={new Date()} estimatedDelivery={est} />
     </div>
   )
 }
