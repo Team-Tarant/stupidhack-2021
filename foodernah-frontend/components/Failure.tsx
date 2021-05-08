89290df86f0e43949a6bd6da173aa05385ae93ef
Hugo Holmqvist <hugo.holm@live.com>
2021-05-08 16:47:09 +0300

    Add customer service system

--- foodernah-frontend/components/Failure.tsx
+++ foodernah-frontend/components/Failure.tsx
@@ -1,23 +1,45 @@
-import { useState } from 'react';
-import styles from './Failure.module.css';
+import { useState } from 'react'
+import styles from './Failure.module.css'
+
+const sendAspa = (phoneNumber: string) =>
+  fetch(
+    `https://stupidhack2021customerservice.lavikjo.com/api/customerservice?number=${encodeURIComponent(
+      phoneNumber
+    )}&pw=KISSATVITTUKOIRIAJAKOIRATVITTUKISSOJA`,
+    { method: 'POST' }
+  )
 
 const Failure = () => {
   const [phoneNumber, setPhoneNumber] = useState<string>('')
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    sendAspa(phoneNumber).then(() => {
+      setPhoneNumber('')
+      alert('Thanks!')
+    })
     // do something here
   }
-  return <div className={styles.container}>
-    <div>
-    <h1>Oh no!</h1>
-    <p>Delivery failed!</p>
-    <p>Please leave you phone number for our special message from our customer service:</p>
-    <form onSubmit={handleSubmit}>
-    <input onChange={(e) => setPhoneNumber(e.target.value)} type="tel" placeholder={'+3581234567'} value={phoneNumber} />
-    <input type="submit" />
-    </form>
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Oh no!</h1>
+        <p>Delivery failed!</p>
+        <p>
+          Please leave you phone number for our special message from our
+          customer service:
+        </p>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={e => setPhoneNumber(e.target.value)}
+            type="tel"
+            placeholder={'+3581234567'}
+            value={phoneNumber}
+          />
+          <input type="submit" />
+        </form>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default Failure
